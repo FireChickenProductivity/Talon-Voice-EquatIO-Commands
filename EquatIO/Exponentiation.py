1a711cb44c67b97b78b8da54ce91a5667c913d6c
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2022-07-16 15:37:42 -0600

    Start to move exponentiation stuff to its own file

---
+++ EquatIO/Exponentiation.py
@@ -0,0 +1,35 @@
+from talon import Module, actions
+    
+mod = Module()
+
+from .VariablePart import VariablePart
+    
+EXPONENTIATION_SPECIFIER = {
+    'squared': '2',
+    'cubed': '3',
+    'inverse': '-1',
+    'transpose': 'T',
+}
+
+PRIME_SYMBOL = "'"
+
+
+
+@mod.capture(rule = 'squared|cubed|inverse|transpose|prime|(to the <user.ordinals>)')
+def equatio_exponentiation_specifier(input) -> VariablePart:
+    if input[0] == 'prime':
+        return VariablePart('prime', PRIME_SYMBOL)
+    input_length = len(input)
+    if input_length == 1:
+        word = EXPONENTIATION_SPECIFIER[input[0]]
+    else:
+        word = str(input.ordinals)
+    return VariablePart('exponentiation', word)
+    
+
+@mod.action_class
+class Actions:
+    def equatio_exponentiation(exponent: VariablePart):
+        '''Inputs the specified exponent into equatio'''
+        actions.user.equatio_exponentiate_text(exponent.text)
+
