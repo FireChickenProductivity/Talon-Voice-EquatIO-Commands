d4480546e39b909ed2fe21bda2be221681cd7d1e
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2022-07-16 15:25:07 -0600

    Delete VariablePart.py

--- VariablePart.py
+++
@@ -1,8 +0,0 @@
-class VariablePart:
-    def __init__(self, type, text):
-        self.type = type
-        self.text = text
-    def __str__(self):
-        return self.text
-    def __repr__(self):
-        return self.__str__()
