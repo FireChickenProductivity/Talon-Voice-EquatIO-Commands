65256edd4a89a745b06171e1a1f8753c52e0529d
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2022-08-15 09:48:18 -0600

    Finish text navigation actions

--- EquatIO/TextFieldNavigation.py
+++ EquatIO/TextFieldNavigation.py
@@ -1,10 +1,61 @@
-from talon import actions, Module
+from talon import actions, Module, clip
 
 from .EquatIO import *
 
+#todo: Clean the considerable repetition in this file
 module = Module()
 @module.action_class
 class Actions:
+
+
+    def equatio_text_navigation_words_left(num_words: int):
+        '''Tries to navigate num_words words to the left using selection to compute the distance'''
+        try:
+            actions.edit.extend_line_start()
+            text = get_selected_text()
+            distance = distance_to_nth_word_left(text,num_words)
+            actions.edit.right()
+            for i in range(distance):
+                actions.edit.left()
+        except ValueError:
+            actions.edit.line_start()
+
+    def equatio_text_select_words_left(num_words: int):
+        '''Tries to select num_words words to the left using selection to compute the distance'''
+        try:
+            actions.edit.extend_line_start()
+            text = get_selected_text()
+            distance = distance_to_nth_word_left(text,num_words)
+            actions.edit.right()
+            for i in range(distance):
+                actions.edit.extend_left()
+        except ValueError:
+            actions.edit.extend_line_start()
+ 
+    def equatio_text_navigation_words_right(num_words: int):
+        '''Tries to navigate num_words words to the right using selection to compute the distance'''
+        try:
+            actions.edit.extend_line_end()
+            text = get_selected_text()
+            distance = distance_to_nth_word_right(text,num_words)
+            actions.edit.left()
+            for i in range(distance):
+                actions.edit.right()
+        except ValueError:
+            actions.edit.line_end()
+
+    def equatio_text_select_words_right(num_words: int):
+        '''Tries to select num_words words to the right using selection to compute the distance'''
+        try:
+            actions.edit.extend_line_end()
+            text = get_selected_text()
+            distance = distance_to_nth_word_right(text,num_words)
+            actions.edit.left()
+            for i in range(distance):
+                actions.edit.extend_right()
+        except ValueError:
+            actions.edit.extend_line_end()
+
     def equatio_text_navigation_word_left():
         '''Tries to navigate one word to the left using selection to compute the distance'''
         try:
@@ -21,7 +72,7 @@ class Actions:
         try:
             actions.edit.extend_line_end()
             text = get_selected_text()
-            distance = distance_to_word_right(text)
+            distance = distance_to_nth_word_right(text)
             actions.edit.left()
             for i in range(distance):
                 actions.edit.right()
@@ -49,3 +100,26 @@ def get_selected_text():
         wait_long_enough_to_let_clipboard_revert_properly()
         result = clip.text()
     return result
+   
+   
+def distance_to_nth_word_left(text: str,num_words:int = 1):
+    words = text.split(" ")
+    if len(words) <= num_words: raise ValueError
+    else:
+        # initalize distance to number of words to include spaces
+        distance = num_words
+        # add the length of the words included
+        for i in range(num_words):
+            distance += len(words[-i-1])
+        return distance
+
+def distance_to_nth_word_right(text: str,num_words:int = 1):
+    words = text.split(" ")
+    if len(words) <= num_words: raise ValueError
+    else:
+        # initalize distance to number of words to include spaces
+        distance = num_words
+        # add the length of the words included
+        for i in range(num_words):
+            distance += len(words[i])
+        return distance
